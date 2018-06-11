facf457e779bbf2b9043f4020d41e756f7173809
Piotr Oleskiewicz <piotr.oleskiewicz@durham.ac.uk>
2018-06-11 19:46:14 +0100

    Fix wrong README type

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os import path
 
 from setuptools import find_packages, setup
 
-with open(path.join(path.abspath(path.dirname(__file__)), "README.md")) as f:
+with open(path.join(path.abspath(path.dirname(__file__)), "README.rst")) as f:
     long_description = f.read()
 
 setup(
