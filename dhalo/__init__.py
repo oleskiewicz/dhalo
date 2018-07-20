41e915b7a0eafd42c03ca2ec0b9de6be4ffb539b
Piotr Oleskiewicz <piotr.oleskiewicz@durham.ac.uk>
2018-07-20 14:36:21 +0100

    Fix imports

--- dhalo/__init__.py
+++ dhalo/__init__.py
@@ -1,12 +1,11 @@
-"""DHalo Reader module.
-"""
 #!/usr/bin/env python3
-import os
-import yaml
 import logging
+import os
+
+import h5py
 import numpy as np
 import pandas as pd
-import h5py
+import yaml
 
 logging.basicConfig(level=logging.DEBUG)
 
